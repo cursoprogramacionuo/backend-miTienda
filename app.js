24fdb340f7f2bd342433eaa10ffa4d6bc757e002
unknown <becario@EII-D202-00.aulasuo.uniovi.es>
2023-07-21 14:10:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const database = require("./database")
+
+const routerItems = require("./routers/routerItems")
+
+const app = express()
+app.use("/items",routerItems)
+
+const port = 3000
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
