a481065d958e0d83e7320266ab96dc92d43d1b5c
unknown <becario@EII-D202-00.aulasuo.uniovi.es>
2023-07-21 14:42:39 +0200

    endpoints for orders

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const database = require("./database")
 
 const routerItems = require("./routers/routerItems")
+const routerOrders = require("./routers/routerOrders")
 
 const app = express()
 app.use("/items",routerItems)
+app.use("/orders",routerOrders)
 
 const port = 3000
 
