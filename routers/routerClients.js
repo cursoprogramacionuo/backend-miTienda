2e091e1776b125778c2c65ebb4a32564e7137dde
unknown <becario@EII-D202-00.aulasuo.uniovi.es>
2023-07-25 11:35:34 +0200

    DELETE /client/:DNI full delete

--- routers/routerClients.js
+++ routers/routerClients.js
@@ -12,8 +12,20 @@ routerClients.delete("/:DNI", async (req, res) => {
     database.connect();
 
     try{
+        let ordersOfClient = await database.query("SELECT id FROM orders WHERE DNIClient = ?",
+            [DNIClient])
+
+        if ( ordersOfClient.length > 0){
+            ordersOfClient = ordersOfClient.map( order => order.id )
+            await database.query("DELETE FROM orders_items WHERE idOrder IN (?)",
+                [ordersOfClient])
+            await database.query("DELETE FROM orders WHERE DNIClient = ?",
+                [DNIClient])
+        }
+
         await database.query("DELETE FROM clients WHERE DNI = ?",
             [DNIClient])
+
     } catch (error){
         return res.status(400).json({ error: "error borrando el usuario" })
     }
